412573ee0121c70c5fe6353b0bcdf72e80231f50
oldbikeguy <kreed002@gmail.com>
2016-05-22 19:55:36 -0600

    Added some error trapping and url testing to verify connectivity before processing. Uploaded a sample of the current_ip file which the script looks for.  Not to worry though. If it's not there, the script should attempt to create the file.  Note, the script must have write access to the directory it will attempt to write to (same directory the script lives in).

--- ddns-update.py
+++ ddns-update.py
@@ -16,9 +16,17 @@
 # import the needed things
 
 import os
+import socket
 from httplib import HTTPSConnection
 from httplib import HTTPConnection
 from base64 import b64encode
+
+def net_test(myUrl):
+	try:
+		socket.getaddrinfo(myUrl, 80)
+		return True
+	except socket.error:
+		return False
 
 # set login info
 username = "---------"
@@ -27,36 +35,53 @@ set = "1"
 
 # store my ip
 IP_FILE = "/home/pi/current_ip"
-f = open(IP_FILE, 'r')
-lastip = f.readline()
-f.close()
-
-# what is my external IP now - 
-# use the ip.changeip.com page 
-# (like what's my ip but much cleaner)
-site = HTTPConnection("ip.changeip.com")
-site.request("GET", "/")
-result = site.getresponse()
-data = result.read()
-currentip = data.split("\n")[0]
-
-# check and update if needed
-if currentip != lastip:
-    	print "Last ip: " + lastip
-    	print "New ip: " + currentip
-
-    	# update the record at changeip.com
-    	upyours = HTTPSConnection("nic.changeip.com")
-    	userAndPass = b64encode(username + ":" + password)
-    	headers = { 'Authorization': 'Basic %s' %  userAndPass, "User-Agent": "rinker.sh wget 1.0" }
-    	upyours.request("POST", "/nic/update?cmd=update&set=" + set, currentip ,headers)
-    	result = upyours.getresponse()
-    	data = result.read()
-    	print "Response: " + data
-
-    	# update the current_ip file
-    	f = open(IP_FILE, "w+")
-    	f.write(current_ip)
-    	f.close()
+if os.path.isfile(IP_FILE):
+	f = open(IP_FILE, 'r')
+	lastip = f.readline()
+	f.close()
+	lastip_set = True
 else:
-    	print "No change"
+	# record the ip if the file doesn't exist
+	f = open(IP_FILE, 'w+')
+	lastip_set = False
+
+
+# test internet connectivity
+if net_test("ip.changeip.com"):
+	# what is my external IP now - 
+	# use the ip.changeip.com page 
+	# (like what's my ip but much cleaner)
+	myUrl = "ip.changeip.com"
+	site = HTTPConnection(myUrl)
+	site.request("GET", "/")
+	result = site.getresponse()
+	data = result.read()
+	currentip = data.split("\n")[0]
+
+	# record ip if no file found
+	if lastip_set == False:
+		f.write(current_ip)
+		f.close()
+
+	# check and update if needed
+	if currentip != lastip:
+    		print "Last ip: " + lastip
+    		print "New ip: " + currentip
+
+    		# update the record at changeip.com
+    		upyours = HTTPSConnection("nic.changeip.com")
+    		userAndPass = b64encode(username + ":" + password)
+    		headers = { 'Authorization': 'Basic %s' %  userAndPass, "User-Agent": "rinker.sh wget 1.0" }
+    		upyours.request("POST", "/nic/update?cmd=update&set=" + set, currentip ,headers)
+    		result = upyours.getresponse()
+    		data = result.read()
+    		print "Response: " + data
+
+    		# update the current_ip file
+    		f = open(IP_FILE, "w+")
+    		f.write(current_ip)
+    		f.close()
+	else:
+    		print "No change"
+else:
+	print "The internet appears to be down.  Will try again later."
